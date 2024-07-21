77303d60caa4ea0476e0a455eed9f65643ec9175
Martin Giessing <mgp@pixlpile.com>
2024-07-21 05:27:53 +0200

    Implemented crossroads, destinations, multiple spawns

--- scenes/red_spawner.gd
+++ scenes/red_spawner.gd
@@ -1,9 +1,17 @@
 extends Node3D
 
-@export var obj_path:PackedScene
+@export var cars_to_spawn: Array[PackedScene]
 @export var end_point:Node3D
+@onready var timer: Timer = $Timer
+var index = 0
 
 func start_simulation():
+	index = 0
+	spawn_car()
+
+
+func spawn_car():
+	var obj_path = cars_to_spawn[index]
 	var loaded_scene = load(obj_path.resource_path)
 	var world = get_parent_node_3d()
 	var instance = loaded_scene.instantiate() as Vehicle
@@ -11,3 +19,11 @@ func start_simulation():
 	instance.rotation = global_rotation
 	instance.set_target(end_point.position)
 	world.add_child(instance)
+	index = index + 1
+	if (index < cars_to_spawn.size()):
+		timer.start()
+
+
+
+func _on_timer_timeout():
+	spawn_car()
--- scripts/vehicle.gd
+++ scripts/vehicle.gd
@@ -14,7 +14,7 @@ var have_set_target = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	direction = (global_transform.basis * Vector3.FORWARD)
 
 
 func set_target(vect):
@@ -45,7 +45,7 @@ func _physics_process(delta):
 					look_at(next_pos)
 				var magnitude = linear_velocity.length()
 				linear_velocity = magnitude * direction
-		apply_force(direction * speed * delta)
+	apply_force(direction * speed * delta)
 		
 	if not navigation.is_target_reachable():
 		print("Can't reach target")
